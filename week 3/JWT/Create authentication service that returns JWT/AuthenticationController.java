34099f35c466aa2ef5b85cb6870a040a9617cf96
Jenifer2833 <jeniferreshma33@gmail.com>
2026-07-12 21:46:17 +0530

    Add files via upload

---
+++ week 3/JWT/Create authentication service that returns JWT/AuthenticationController.java
@@ -0,0 +1,31 @@
+package com.cognizant.authenticationservice.controller;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.authenticationservice.model.AuthenticationResponse;
+import com.cognizant.authenticationservice.util.JwtUtil;
+
+@RestController
+public class AuthenticationController {
+
+    @GetMapping("/authenticate")
+    public AuthenticationResponse authenticate(@RequestHeader("Authorization") String authHeader) {
+
+        String base64Credentials = authHeader.substring(6);
+
+        byte[] decodedBytes = Base64.getDecoder().decode(base64Credentials);
+
+        String credentials = new String(decodedBytes, StandardCharsets.UTF_8);
+
+        String username = credentials.split(":")[0];
+
+        String token = JwtUtil.generateToken(username);
+
+        return new AuthenticationResponse(token);
+    }
+}
